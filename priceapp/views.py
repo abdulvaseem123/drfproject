a5160d6cbbc740e5fa2bca5fc97e78f2b776c2ff
vaseem <vaseem@navedas.com>
2020-08-23 15:46:17 +0530

    made the required UI changes

--- priceapp/views.py
+++ priceapp/views.py
@@ -39,79 +39,11 @@ class PricingPlanDeleteView(DeleteView):
 
 def index(request):
     all_users=PricingPlan.objects.all()
-    #all_users = list(all_users)
     pricelist=PricingPlanOption.objects.all() 
-    #print("pricelistttttttttttt",pricelist)
     price_list=[]
-    # for price in pricelist:
-    #     price_list.append(price.price)
-    # price_list_dict={'price_list':price_list}
-    # all_users.append(price_list_dict)    
-    # print(price_list)     
     context={'all_users':all_users,'price_list':price_list}
     return render(request,'priceapp/index.html',context)        
-    #return render(request,'/src/index.html')
-# class PricingPlanList(APIView):
-#     def get(self,request):
-#         PricingPlan_obj=PricingPlan.objects.all()
-#         serializer=PricingPlanSerializer(PricingPlan_obj,many=True)
-#         return Response(serializer.data)
-
-#     def post(self,request):
-#         serializer = PricingPlanSerializer(data=request.data,context={"request": request.data})
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-# class PricingPlanOptionList(APIView):
-#     def get(self,request):
-#         PricingPlanOption_obj=PricingPlanOption.objects.all()
-#         serializer=PricingPlanOptionSerializer(PricingPlanOption_obj,many=True)
-#         return Response(serializer.data)
-
-#     def post(self,request):
-#         pass     
-
-# class PricingDelhiList(APIView):
-#     def get(self,request):
-#         obj_one=PricingPlan.objects.all()
-#         serializer=PricingDelhiSerializer(obj_one,many=True)
-#         return Response(serializer.data)
-#     def post(self,request):
-#         pass     
-
-# class PricingHyderabadList(APIView):
-#     def get(self,request):
-#         obj_one=PricingPlanOption.objects.all()
-#         serializer=PricingHyderabadSerializer(obj_one,many=True)
-#         return Response(serializer.data)
-#     def post(self,request):
-#         pass     
+   
 
 
-def create(request):
-    if request.method == 'POST':
-        form = PricingPlanForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('index')
-    form = PricingPlanForm()
-
-    return render(request,'priceapp/create.html',{'form': form})
-
-def edit(request, pk, template_name='priceapp/edit.html'):
-    contact = get_object_or_404(PricingPlan, pk=pk)
-    form = PricingPlanForm(request.POST or None, instance=post)
-    if form.is_valid():
-        form.save()
-        return redirect('index')
-    return render(request, template_name, {'form':form})
-
-def delete(request, pk, template_name='priceapp/confirm_delete.html'):
-    contact = get_object_or_404(PricingPlan, pk=pk)
-    if request.method=='POST':
-        contact.delete()
-        return redirect('index')
-    return render(request, template_name, {'object':contact})
 
